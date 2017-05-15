e03c71b7326990f4cb93e656e2a2b701e188a0b3
cyclon1978 <cyclon1978@web.de>
2017-05-15 13:33:44 +0200

    cleanup: removed implicit packages
    Change-Id: I1a6b4fba18ba8f8f1b687afe87ba9cc276e5509e

--- device.mk
+++ device.mk
@@ -103,32 +103,10 @@ PRODUCT_COPY_FILES += \
     hardware/qcom/audio-caf/msm8952/configs/msm8952_32/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml \
     $(LOCAL_PATH)/audio/configs/mixer_paths_qrd_skun.xml:system/etc/mixer_paths_qrd_skun.xml
 
-## PRODUCT_COPY_FILES += \
-##     $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
-##     $(LOCAL_PATH)/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
-##     $(LOCAL_PATH)/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
-##     $(LOCAL_PATH)/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
-##     $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
-##     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths.xml:system/etc/sound_trigger_mixer_paths.xml \
-##     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9306.xml:system/etc/sound_trigger_mixer_paths_wcd9306.xml \
-##     $(LOCAL_PATH)/audio/sound_trigger_mixer_paths_wcd9330.xml:system/etc/sound_trigger_mixer_paths_wcd9330.xml \
-##     $(LOCAL_PATH)/audio/sound_trigger_platform_info.xml:system/etc/sound_trigger_platform_info.xml
-
-# Browser
-PRODUCT_PACKAGES += \
-    Jelly
-#    Gello
-
 # Camera
 PRODUCT_PACKAGES += \
-    Snap \
-	libmmcamera_interface \
-	libmmjpeg_interface \
-	libmm-qcamera \
     camera.msm8952 \
-    libqomx_core
-    
-#    Camera2 \
+    Snap
 
 # Connectivity Engine support (CNE)
 PRODUCT_PACKAGES += \
@@ -270,8 +248,7 @@ PRODUCT_PACKAGES += \
 
 # audio
 PRODUCT_PACKAGES += \
-	libqct_resampler \
-    libaudioresampler
+	libqct_resampler
 
 PRODUCT_PACKAGES += \
     p2p_supplicant_overlay.conf \
